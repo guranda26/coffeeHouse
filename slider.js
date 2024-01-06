53155eb65cacc4f55326e3fd83ef7833a7a6ab3d
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-01-06 21:47:23 +0400

    feat: responsiveness

--- slider.js
+++ slider.js
@@ -41,7 +41,7 @@ function showSlide(index) {
   article.innerHTML = `
     <img src="${slider[index].source}" alt="${slider[index].thumbnail}" />
     <h3>${slider[index].title}</h3>
-    <p>${slider[index].description}</p>
+    <p class="description>${slider[index].description}</p>
     <p>${slider[index].price}</p>
   `;
 
