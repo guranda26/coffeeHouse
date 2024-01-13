19960a79fa5ec72e6337c56053efc7bb944a9382
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-01-13 23:19:53 +0400

    fix: relative path to images

--- menu.js
+++ menu.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const dessertContainer = document.querySelector("#dessert-offer");
 
   const images = Array.from({ length: 8 }, (_, index) => ({
-    src: `../assets/coffee-menu-${index + 1}.jpeg`,
+    src: `./assets/coffee-menu-${index + 1}.jpeg`,
   }));
   const teaImages = Array.from({ length: 4 }, (_, index) => ({
-    src: `../assets/tea-${index + 1}.png`,
+    src: `./assets/tea-${index + 1}.png`,
   }));
   const dessertImages = Array.from({ length: 8 }, (_, index) => ({
-    src: `../assets/dessert-${index + 1}.png`,
+    src: `./assets/dessert-${index + 1}.png`,
   }));
 
   fetch("./products.json")
